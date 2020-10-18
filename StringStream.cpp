5810b594200386e6ecd0a12afa142eae23995c0b
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-10-18 21:00:01 +0530

    Create StringStream.cpp

---
+++ StringStream.cpp
@@ -0,0 +1,38 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+
+      vector <int>v;  
+      stringstream ss(str);
+char ch;
+int a;
+while(ss)
+{
+    if(ss.peek()!=',')
+    {
+        if(ss>>a){
+            v.push_back(a);
+    
+        }
+        }
+    else{
+        ss>>ch;
+    }
+}
+return v;
+
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
